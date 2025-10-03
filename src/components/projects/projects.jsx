0b3dce6a2092b2d7ae50be2767c52808b2a3a637
Minyoui <evanlyrush73@gmail.com>
2025-10-03 14:30:31 +0800

    6th Commit (Projects Update)

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,10 +1,33 @@
 import './projects.css';
 
 function Projects () {
+    const projectsData = [
+        {
+            id: 'project-one',
+            image: '',
+            title: 'Maison`D',
+            description: 'A modern website developed to provide an online booking system for a small business. ',
+            role: 'Front-End Developer',
+            status: 'Deployed / Live',
+            link: '',
+            tags: ['','']
+        }
+    ];
+
     return (
         <>
-            <div>
-                
+            <div className='projects-section' id="projects">
+                {projectsData.map((project) => (
+                    <div className='project-box' id={project.id} key={project.id}>
+                        <div className='project-info'>
+                            <h1>{project.title}</h1>
+                            <p>{project.description}</p>
+                            <div className='line'/>
+                            <p>Role:{project.role}</p>
+                            <p>Status:{project.status}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </>
     )
